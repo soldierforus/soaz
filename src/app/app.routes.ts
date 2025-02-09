525800feb51f0647bf53dc720210087d98f1c588
soldierforus <travis@tdbennett.com>
2025-02-09 05:27:56 -0700

    tucson-weekly

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { DesignsComponent } from './designs/designs.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'tucson-weekly', component: HomeComponent },
   { path: 'designs', component: DesignsComponent },
   { path: 'designs/live-demo', component: DesignsComponent },
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
